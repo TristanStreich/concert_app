e28244b97bdb023485d9e0485fe71d54eebac649
Tristan Streichenberger <33437952+TristanStreich@users.noreply.github.com>
2026-01-23 14:37:58 -0800

    speed up docker builds (#4)

--- docker-build.sh
+++ docker-build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-VERSION=0.2.1
+VERSION=0.2.2
 
 DOCKER_NAME=registry.ferris.place/concert-list
 
 VERSION_TAG="$DOCKER_NAME:$VERSION"
 LATEST_TAG="$DOCKER_NAME:latest"
+
+# Enable BuildKit for faster builds and cache mounts
+export DOCKER_BUILDKIT=1
 
 docker build -t $VERSION_TAG -t $LATEST_TAG .
 
